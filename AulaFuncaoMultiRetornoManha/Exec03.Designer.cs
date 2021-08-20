8dca24d38d32983bbf879cf449d78b26666e3478
009Schmitt <34982675+009Schmitt@users.noreply.github.com>
2021-08-20 09:41:28 -0300

    Primeiro Commit

---
+++ AulaFuncaoMultiRetornoManha/Exec03.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace AulaFuncaoMultiRetornoManha
+{
+    partial class Exec03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgHistorico = new System.Windows.Forms.DataGridView();
+            this.IDCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValorEmCaixaCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DespesasDoMesCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GanhosDoMesCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValorLiquidoCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SeLucrouCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGanhos = new System.Windows.Forms.TextBox();
+            this.txtDespesas = new System.Windows.Forms.TextBox();
+            this.btnCalcula = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgHistorico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgHistorico
+            // 
+            this.dgHistorico.AllowUserToAddRows = false;
+            this.dgHistorico.AllowUserToDeleteRows = false;
+            this.dgHistorico.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgHistorico.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IDCol,
+            this.ValorEmCaixaCol,
+            this.DespesasDoMesCol,
+            this.GanhosDoMesCol,
+            this.ValorLiquidoCol,
+            this.SeLucrouCol});
+            this.dgHistorico.Location = new System.Drawing.Point(12, 12);
+            this.dgHistorico.Name = "dgHistorico";
+            this.dgHistorico.ReadOnly = true;
+            this.dgHistorico.RowTemplate.Height = 50;
+            this.dgHistorico.Size = new System.Drawing.Size(1260, 325);
+            this.dgHistorico.TabIndex = 0;
+            // 
+            // IDCol
+            // 
+            this.IDCol.HeaderText = "ID";
+            this.IDCol.Name = "IDCol";
+            this.IDCol.ReadOnly = true;
+            this.IDCol.Width = 70;
+            // 
+            // ValorEmCaixaCol
+            // 
+            this.ValorEmCaixaCol.HeaderText = "ValorEmCaixa";
+            this.ValorEmCaixaCol.Name = "ValorEmCaixaCol";
+            this.ValorEmCaixaCol.ReadOnly = true;
+            this.ValorEmCaixaCol.Width = 250;
+            // 
+            // DespesasDoMesCol
+            // 
+            this.DespesasDoMesCol.HeaderText = "DespesasDoMes";
+            this.DespesasDoMesCol.Name = "DespesasDoMesCol";
+            this.DespesasDoMesCol.ReadOnly = true;
+            this.DespesasDoMesCol.Width = 270;
+            // 
+            // GanhosDoMesCol
+            // 
+            this.GanhosDoMesCol.HeaderText = "GanhosDoMes";
+            this.GanhosDoMesCol.Name = "GanhosDoMesCol";
+            this.GanhosDoMesCol.ReadOnly = true;
+            this.GanhosDoMesCol.Width = 250;
+            // 
+            // ValorLiquidoCol
+            // 
+            this.ValorLiquidoCol.HeaderText = "ValorLiquido";
+            this.ValorLiquidoCol.Name = "ValorLiquidoCol";
+            this.ValorLiquidoCol.ReadOnly = true;
+            this.ValorLiquidoCol.Width = 210;
+            // 
+            // SeLucrouCol
+            // 
+            this.SeLucrouCol.HeaderText = "SeLucrou";
+            this.SeLucrouCol.Name = "SeLucrouCol";
+            this.SeLucrouCol.ReadOnly = true;
+            this.SeLucrouCol.Width = 180;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(1225, 343);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(47, 206);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ganhos do Mes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 453);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Despesa do Mes";
+            // 
+            // txtGanhos
+            // 
+            this.txtGanhos.Location = new System.Drawing.Point(294, 389);
+            this.txtGanhos.Name = "txtGanhos";
+            this.txtGanhos.Size = new System.Drawing.Size(295, 47);
+            this.txtGanhos.TabIndex = 4;
+            // 
+            // txtDespesas
+            // 
+            this.txtDespesas.Location = new System.Drawing.Point(294, 450);
+            this.txtDespesas.Name = "txtDespesas";
+            this.txtDespesas.Size = new System.Drawing.Size(295, 47);
+            this.txtDespesas.TabIndex = 5;
+            // 
+            // btnCalcula
+            // 
+            this.btnCalcula.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcula.Location = new System.Drawing.Point(595, 343);
+            this.btnCalcula.Name = "btnCalcula";
+            this.btnCalcula.Size = new System.Drawing.Size(624, 206);
+            this.btnCalcula.TabIndex = 6;
+            this.btnCalcula.Text = "Calcula Mês";
+            this.btnCalcula.UseVisualStyleBackColor = true;
+            this.btnCalcula.Click += new System.EventHandler(this.btnCalcula_Click);
+            // 
+            // Exec03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 561);
+            this.Controls.Add(this.btnCalcula);
+            this.Controls.Add(this.txtDespesas);
+            this.Controls.Add(this.txtGanhos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.dgHistorico);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Exec03";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exec03";
+            this.Load += new System.EventHandler(this.Exec03_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgHistorico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgHistorico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValorEmCaixaCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DespesasDoMesCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GanhosDoMesCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValorLiquidoCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SeLucrouCol;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGanhos;
+        private System.Windows.Forms.TextBox txtDespesas;
+        private System.Windows.Forms.Button btnCalcula;
+    }
+}
---
+++ AulaFuncaoMultiRetornoManha/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace AulaFuncaoMultiRetornoManha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rbExec03 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.btnVai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(231, 43);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 61);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(231, 43);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // rbExec03
+            // 
+            this.rbExec03.AutoSize = true;
+            this.rbExec03.Location = new System.Drawing.Point(249, 12);
+            this.rbExec03.Name = "rbExec03";
+            this.rbExec03.Size = new System.Drawing.Size(167, 43);
+            this.rbExec03.TabIndex = 2;
+            this.rbExec03.TabStop = true;
+            this.rbExec03.Text = "EXEC03";
+            this.rbExec03.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(249, 61);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(231, 43);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // btnVai
+            // 
+            this.btnVai.BackColor = System.Drawing.Color.Gold;
+            this.btnVai.Font = new System.Drawing.Font("Microsoft Sans Serif", 250F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVai.ForeColor = System.Drawing.Color.Red;
+            this.btnVai.Location = new System.Drawing.Point(12, 110);
+            this.btnVai.Name = "btnVai";
+            this.btnVai.Size = new System.Drawing.Size(760, 439);
+            this.btnVai.TabIndex = 4;
+            this.btnVai.Text = "VAI";
+            this.btnVai.UseVisualStyleBackColor = false;
+            this.btnVai.Click += new System.EventHandler(this.btnVai_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnVai);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.rbExec03);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rbExec03;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button btnVai;
+    }
+}
+
